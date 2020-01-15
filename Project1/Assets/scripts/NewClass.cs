9afb61545572f33791f8ac637ae85a1e3bea3f69
Jayme Withers <10859571@my.uvu.edu>
2020-01-15 10:51:53 -0700

    variable lecture

---
+++ Project1/Assets/scripts/NewClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    
+    // Start is called before the first frame update
+    public void Start()
+    {
+        print(nameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
