99048d028aa3270cd8d565da025803a84ffc3e16
Jayme Withers <10859571@my.uvu.edu>
2020-01-22 10:49:33 -0700

    1/22 lecture

---
+++ Project1/Assets/scripts/Newer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Newer : MonoBehaviour
+{
+
+    public float floatNumber;
+    public int scoreValue;
+    
+    void Start()
+    {
+        print(scoreValue);
+    }
+    
+    void Update()
+    {
+        print((floatNumber));
+    }
+}
