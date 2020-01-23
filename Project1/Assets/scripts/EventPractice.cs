a9327d427afe937eaf6dc38f0c6c456e823b26ef
Jayme Withers <10859571@my.uvu.edu>
2020-01-23 11:15:13 -0700

    playing with unity events

---
+++ Project1/Assets/scripts/EventPractice.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventPractice : MonoBehaviour
+{
+
+    public UnityEvent uEventOne;
+    
+    void Start()
+    {
+     uEventOne.Invoke();   
+    }
+}
