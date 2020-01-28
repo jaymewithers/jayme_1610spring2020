a4528642df67cafbb632ae72a757a2fc521e1b69
Jayme Withers <10859571@my.uvu.edu>
2020-01-28 11:13:52 -0700

    transform and rotate tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/TransformFunctions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class TransformFunctions : MonoBehaviour
+    {
+        public float moveSpeed = 10f;
+        public float turnSpeed = 50f;
+        
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
+
+            if (Input.GetKey(KeyCode.DownArrow))
+                transform.Translate(-Vector3.forward * (moveSpeed * Time.deltaTime));
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+                transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.RightArrow))
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    }
+}
+
+// unity tutorial - transform and rotate
