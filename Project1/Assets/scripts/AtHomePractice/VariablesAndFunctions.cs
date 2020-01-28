805e946062153b4cd2163a5c8fc88b3b24161735
Jayme Withers <10859571@my.uvu.edu>
2020-01-28 11:04:41 -0700

    variables and functions tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/VariablesAndFunctions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    private int myInt = 7;
+    
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
+
+// unity tutorial - variables and functions
