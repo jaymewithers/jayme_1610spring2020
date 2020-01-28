153dff1718b4fa55a7fc6f3ddf5b167416416af4
Jayme Withers <10859571@my.uvu.edu>
2020-01-27 20:10:41 -0700

    OnTriggerStay and OnTriggerExit

---
+++ Project1/Assets/scripts/Homework/TriggerEventsStayExit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Homework
+{
+    public class TriggerEventsStayExit : MonoBehaviour
+    {
+        public void OnTriggerStay(Collider other)
+        {
+            if (other.attachedRigidbody)
+            {
+                print("Attack!");
+            }
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+// help from unity's website
