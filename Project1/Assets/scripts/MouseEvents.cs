5ff7e8c5bb3e2b385bfe31e2065dd60b3fe5df3a
Jayme Withers <10859571@my.uvu.edu>
2020-01-29 11:19:33 -0700

    onmouse events

---
+++ Project1/Assets/scripts/MouseEvents.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+
+    public UnityEvent mouseDownEvent,
+        mouseOverEvent,
+        mouseExitEvent,
+        mouseDragEvent,
+        mouseUpButtonEvent,
+        mouseEnterEvent,
+        mouseUpEvent;
+
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        mouseUpButtonEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+}
---
+++ Project1/Assets/scripts/TriggeringEvents.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public int intNumber;
+    public float floatNumber;
+    public string stringNumber;
+
+    public UnityEvent triggeringEnterEvent;
+    public UnityEvent triggeringExitEvent;
+    public UnityEvent triggeringStayEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody)
+        {
+            triggeringEnterEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.attachedRigidbody)
+        {
+            triggeringExitEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggeringStayEvent.Invoke();
+    }
+}
