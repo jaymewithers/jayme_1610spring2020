b10d13e7cfc14d0738bc2e5c4bd78d45b3aa3583
Jayme Withers <10859571@my.uvu.edu>
2020-01-30 15:20:45 -0700

    using DeltaTime tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/UsingDeltaTime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class UsingDeltaTime : MonoBehaviour
+    {
+        public float speed = 8f;
+        public float countdown = 3.0f;
+        public Light enableLight;
+
+        void Update()
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0.0f)
+                enableLight.enabled = true;
+
+            if (Input.GetKey(KeyCode.RightArrow))
+                transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+            {
+                
+            }
+        }
+    }
+}
+
+// unity tutorial - using DeltaTimes with slight modifications
