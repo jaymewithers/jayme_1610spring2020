0c3fa31fbabd71c092c7422f38a75f914c55d98c
Jayme Withers <10859571@my.uvu.edu>
2020-01-30 11:01:51 -0700

    key code experiments

---
+++ Project1/Assets/scripts/AtHomePractice/KeyCodeExperiments.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class KeyCodeExperiments : MonoBehaviour
+    {
+        public Transform target;
+        public GameObject other;
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.H))
+            {
+                print("Hello World!");
+            }
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                print("Look at that!");
+                transform.LookAt(target);
+            }
+
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                print("Away with you!");
+                Destroy(other);
+            }
+        }
+    }
+}
