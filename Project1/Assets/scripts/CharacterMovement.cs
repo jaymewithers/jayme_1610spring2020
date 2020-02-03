50682c2dc889c2c35653cba0eb576fc5d9e3644b
Jayme Withers <10859571@my.uvu.edu>
2020-02-03 12:59:19 -0700

    character movement - class lecture

---
+++ Project1/Assets/scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+
+    
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal") * speed;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+            
+        {
+            positionDirection.y = jumpForce;
+        }
+
+        positionDirection.y = gravity;
+        controller.Move(positionDirection * Time.deltaTime);
+    }
+}
