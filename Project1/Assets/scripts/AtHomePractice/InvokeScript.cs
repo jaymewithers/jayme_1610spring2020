94374b178e52ea3f7d3ac05239a4b86bc1e125d0
Jayme Withers <10859571@my.uvu.edu>
2020-02-08 11:45:28 -0700

    invoke repeating tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/InvokeScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace AtHomePractice
+{
+    public class InvokeScript : MonoBehaviour
+    {
+        public GameObject target;
+
+        
+        void Start()
+        {
+            InvokeRepeating("SpawnObject", 2, 1);
+        }
+
+        
+        void SpawnObject()
+        {
+            float x = Random.Range(-4.0f, 4.0f);
+            float z = Random.Range(-4.0f, 4.0f);
+            Instantiate(target, new Vector3(x, 5, z), Quaternion.identity);
+        }
+    }
+}
+
+// unity tutorial - invoke
