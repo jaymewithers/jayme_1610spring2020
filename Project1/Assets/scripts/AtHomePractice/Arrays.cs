716f58974bd1ea78e13fd0f7e3c37df4229ed887
Jayme Withers <10859571@my.uvu.edu>
2020-02-07 20:47:46 -0700

    Arrays tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/Arrays.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class Arrays : MonoBehaviour
+    {
+        public GameObject[] players;
+    
+        void Start()
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+            
+            
+              for (int i = 0; i < players.Length; i++)
+              {
+                  Debug.Log("Player Number " + i + " is named" + players[i].name);
+              }
+        }
+    }
+}
+
+// unity tutorial - arrays
