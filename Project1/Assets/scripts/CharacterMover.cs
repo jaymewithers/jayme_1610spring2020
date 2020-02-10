da5bb041bba5f282ef9dc0eb584c8270152d3fa0
Jayme Withers <10859571@my.uvu.edu>
2020-02-10 11:13:54 -0700

    if statements / rigidbodies / components / scope

--- Project1/Assets/scripts/CharacterMover.cs
+++ Project1/Assets/scripts/CharacterMover.cs
@@ -1,26 +1,35 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
 
-    public CharacterController controller;
+    private CharacterController controller;
     public Vector3 positionDirection;
     public float speed = 10f;
     public float gravity = 3f;
     public float jumpForce = 3f;
+    private int jumpCount = 0;
+    public int jumpCountMax = 2;
 
     void Start()
     {
-     
+        controller = GetComponent<CharacterController>();
     }
     
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
         positionDirection.x = Input.GetAxis("Horizontal") * speed;
        
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount <= jumpCountMax)
         {
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
         
         positionDirection.y -= gravity;
