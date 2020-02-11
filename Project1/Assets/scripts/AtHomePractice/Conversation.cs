25ec5f28cab642cabaab4761a2d856a3fe2dce33
Jayme Withers <10859571@my.uvu.edu>
2020-02-11 10:18:50 -0700

    switch statements tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/Conversation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class Conversation : MonoBehaviour
+    {
+        public int intelligence = 5;
+
+        public void Start()
+        {
+            switch (intelligence)
+            {
+                case 5:
+                    print("Top of the morning to you, good sir.");
+                    break;
+                case 4:
+                    print("Hello! How are you?");
+                    break;
+                case 3:
+                    print("Hey pal. What's up?");
+                    break;
+                case 2:
+                    print("Sup loser.");
+                    break;
+                case 1:
+                    print("Blah bloo ulg blib.");
+                    break;
+                default:
+                    print("Incorrect intelligence level.");
+                    break;
+            }
+        }
+    }
+}
+
+// unity tutorial - switch statements
