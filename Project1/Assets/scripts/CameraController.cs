31f7148dab703e890408288054e27e7194314a6a
Jayme Withers <10859571@my.uvu.edu>
2020-02-12 11:17:39 -0700

    writing to components / sprites / onEnable

---
+++ Project1/Assets/scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+
+    private Camera cameraObj;
+    void Start()
+    {
+        cameraObj = GetComponent<Camera>();
+        cameraObj.backgroundColor = Color.yellow;
+        cameraObj.fieldOfView = 70f;
+    }
+}
---
+++ Project1/Assets/scripts/SpriteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteController : MonoBehaviour
+{
+    private SpriteRenderer spriteObj;
+    public Color spriteColor = Color.red;
+    public Color offColor = Color.red;
+    
+    void Start()
+    {
+        spriteObj = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        spriteObj.color = spriteColor;
+    }
+
+    private void OnDisable()
+    {
+        spriteObj.color = offColor;
+    }
+}
