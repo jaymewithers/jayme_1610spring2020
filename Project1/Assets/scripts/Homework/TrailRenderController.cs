846964b3161c5f3549df4a2c80062e768aee404b
Jayme Withers <10859571@my.uvu.edu>
2020-02-13 11:10:29 -0700

    button and trail renderer

---
+++ Project1/Assets/scripts/Homework/TrailRenderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Homework
+{
+    [RequireComponent(typeof(TrailRenderer))]
+    public class TrailRenderController : MonoBehaviour
+    {
+        private TrailRenderer trailRenderControl;
+        public Material trailRenderMaterial;
+        
+        void Start()
+        {
+            trailRenderControl = GetComponent<TrailRenderer>();
+            trailRenderControl.time = 2f;
+            trailRenderControl.endWidth = 0.1f;
+            trailRenderControl.startWidth = 1f;
+            trailRenderMaterial.color = Color.green;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.B))
+            {
+                trailRenderMaterial.color = Color.blue;
+            }
+        }
+    }
+}
