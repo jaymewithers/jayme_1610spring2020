8dc53565d5f5bba741384213bec8fbf0dc719cdb
Jayme Withers <10859571@my.uvu.edu>
2020-02-13 10:36:02 -0700

    button controls

---
+++ Project1/Assets/scripts/Homework/ButtonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Homework
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonController : MonoBehaviour
+    {
+        private Button buttonObj;
+
+        void Start()
+        {
+            buttonObj = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            print("Click the button!");
+        }
+
+        private void OnDisable()
+        {
+            print("No button pressing for you.");
+        }
+    }
+}
