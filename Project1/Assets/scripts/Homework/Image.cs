e9b055409151cdef90cc88cd78028b2e92402b5e
Jayme Withers <10859571@my.uvu.edu>
2020-02-15 18:16:08 -0700

    image component (keep working with it)

---
+++ Project1/Assets/scripts/Homework/Image.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Homework
+{
+    [RequireComponent(typeof(Image))]
+    public class Image : MonoBehaviour
+    {
+        private Image imageControl;
+        public Material imageMaterial;
+        
+        void Start()
+        {
+            imageControl = GetComponent<Image>();
+            imageMaterial.color = Color.red;
+        }
+
+        private void OnEnable()
+        {
+            print("There is an image.");
+        }
+
+        private void OnDisable()
+        {
+            print("No image.");
+        }
+    }
+}
