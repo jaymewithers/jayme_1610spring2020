59d56976e0dc5ad50fa28d6ceab951c1a3d2ec40
Jayme Withers <10859571@my.uvu.edu>
2020-02-16 19:58:38 -0700

    text controller

---
+++ Project1/Assets/scripts/Homework/TextController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Homework
+{
+    [RequireComponent(typeof(Text))]
+    public class TextController : MonoBehaviour
+    {
+        private Text textControl;
+
+        void Start()
+        {
+            textControl = GetComponent<Text>();
+            textControl.alignment = TextAnchor.MiddleCenter;
+            textControl.color = Color.green;
+            textControl.text = "Hello";
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                textControl.text = "Welcome!";
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                textControl.text = "Run, run, run!";
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                textControl.text = "Go, go, go!";
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                textControl.text = "Jump!";
+            }
+        }
+    }
+}
