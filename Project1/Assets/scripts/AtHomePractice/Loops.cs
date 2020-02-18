d2f5e27ef12352ba616eb28fc571a8fd16bb2a70
Jayme Withers <10859571@my.uvu.edu>
2020-02-17 17:49:20 -0700

    loops tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/Loops.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class Loops : MonoBehaviour
+    {
+        public int cupsInTheSink = 4;
+        
+        void Start()
+        {
+            while (cupsInTheSink > 0)
+            {
+                Debug.Log("I've washed a cup!");
+                cupsInTheSink--;
+            }
+
+            bool shouldContinue = false;
+
+            do
+            {
+              print("Hello World");  
+              
+            } while (shouldContinue == true);
+            
+            string[] strings = new string[3];
+
+            strings[0] = "First string";
+            strings[1] = "Second string";
+            strings[2] = "Third string";
+
+            foreach (string item in strings)
+            {
+                print(item);
+            }
+        }
+    }
+}
+
+// unity tutorial - loops
