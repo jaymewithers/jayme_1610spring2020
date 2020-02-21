4bb82a842f90feeb3cd7993c0fd4a62c37b2de52
Jayme Withers <10859571@my.uvu.edu>
2020-02-20 19:33:08 -0700

    character controller practice

---
+++ Project1/Assets/scripts/AtHomePractice/CharacterMoverPractice.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class CharacterMoverPractice : MonoBehaviour
+    {
+        public CharacterController controller;
+        public float speed = 10f;
+        public float gravity = 3f;
+        public float jumpForce = 3f;
+        public Vector3 positionDirection;
+        private int jumpCount = 0;
+        public int jumpCountMax = 2;
+    
+        void Start()
+        {
+          
+        }
+
+    
+        void Update()
+        {
+            if (controller.isGrounded)
+            {
+                positionDirection.y = 0;
+                jumpCount = 0;
+            }
+            
+            positionDirection.x = Input.GetAxis("Horizontal") * speed;
+            
+            if (Input.GetButtonDown("Jump") && jumpCount <= jumpCountMax)
+            {
+                positionDirection.y = jumpForce;
+                jumpCount++;
+            }
+            
+            positionDirection.y -= gravity;
+            controller.Move(positionDirection * Time.deltaTime);
+        }
+    }
+}
