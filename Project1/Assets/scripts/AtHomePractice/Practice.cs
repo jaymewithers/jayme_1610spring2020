00b77989b696f837ef489b0c91096b911c9883e2
Jayme Withers <10859571@my.uvu.edu>
2020-02-23 15:30:33 -0700

    auto movement practice controls

---
+++ Project1/Assets/scripts/AtHomePractice/Practice.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+[RequireComponent(typeof(CharacterController))]
+    public class Practice : MonoBehaviour
+    {
+        private CharacterController controller;
+        private Vector3 positionDirection;
+        private float gravity = 3f;
+        void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+
+   
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Backspace))
+            {
+                positionDirection.x = 0;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                positionDirection.x = -5;
+            }
+
+            if (Input.GetKey(KeyCode.RightShift))
+            {
+                positionDirection.x = 5;
+            }
+
+            positionDirection.y -= gravity;
+            controller.Move(positionDirection * Time.deltaTime);
+        }
+    }
+}
