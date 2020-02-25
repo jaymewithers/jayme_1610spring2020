bd146461071f83feccbc4dd192b51795a0fbcd9e
Jayme Withers <10859571@my.uvu.edu>
2020-02-25 10:28:19 -0700

    auto moving object

---
+++ Project1/Assets/scripts/AtHomePractice/MovingWall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class MovingWall : MonoBehaviour
+    {
+        public float min = 2f;
+        public float max = 3f;
+
+        void Start()
+        {
+            min = transform.position.x;
+            max = transform.position.x + 8;
+        }
+
+
+        void Update()
+        {
+            transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y,
+                transform.position.z);
+        }
+    }
+}
+
+// help from unity forum (user parandham03)
