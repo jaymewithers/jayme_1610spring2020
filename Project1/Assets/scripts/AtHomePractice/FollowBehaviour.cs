ad19a7419533e0a2b94f75e5d6267822da973b96
Jayme Withers <10859571@my.uvu.edu>
2020-02-27 10:39:07 -0700

    object following another object

---
+++ Project1/Assets/scripts/AtHomePractice/FollowBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class FollowBehaviour : MonoBehaviour
+    {
+        private Rigidbody rigidbodyObj;
+        public float movementSpeed = 5;
+        public Transform target;
+        private float maxDistance = 1.5f;
+        
+        void Start()
+        {
+            rigidbodyObj = GetComponent<Rigidbody>();
+        }
+        
+        void Update()
+        {
+            transform.LookAt(target.position);
+            if ((transform.position - target.position).magnitude > maxDistance)
+                transform.Translate(0.0f, 0.0f, movementSpeed * Time.deltaTime);
+            
+        }
+    }
+}
+
+// help from unity tutorial - following an object
