be1bd4ced3dd1a3b3d4ed9d3cb3f3b2068adb3fa
Jayme Withers <10859571@my.uvu.edu>
2020-02-29 11:34:24 -0700

    flying player

---
+++ Project1/Assets/scripts/AtHomePractice/Flight.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Flight : MonoBehaviour
+    {
+        private CharacterController controller;
+        public float flightSpeed = 4f;
+        public Vector3 positionDirection;
+        public float speed = 3f;
+        public float flightForce = 5f;
+        public float gravity = 0.02f;
+
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+
+        public virtual void ChangePosition()
+        {
+            transform.position += new Vector3(0, flightSpeed, 0);
+        }
+
+        private void Update()
+        {
+            ChangePosition();
+            positionDirection.x = Input.GetAxis("Horizontal") * speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                positionDirection.y = flightForce;
+            }
+
+            positionDirection.y -= gravity;
+            controller.Move(positionDirection * Time.deltaTime);
+        }
+    }
+}
