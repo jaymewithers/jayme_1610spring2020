664818a9c59a3bcc7bd05a1e3dfce32b37ee4e36
Jayme Withers <10859571@my.uvu.edu>
2020-03-01 14:14:54 -0700

    enemy follow

---
+++ Project1/Assets/scripts/AtHomePractice/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Enemy : MonoBehaviour
+    {
+        public float speed = 3f;
+        private Rigidbody enemyRb;
+        private GameObject player;
+
+        void Start()
+        {
+            enemyRb = GetComponent<Rigidbody>();
+            player = GameObject.Find("Player");
+        }
+
+
+        void Update()
+        {
+            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+            enemyRb.AddForce(lookDirection * speed);
+        }
+    }
+}
+
+// unity tutorial
