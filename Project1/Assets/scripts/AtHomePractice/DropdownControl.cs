472486c226e5b89e10c6438dd9392f8cfc3ace83
Jayme Withers <10859571@my.uvu.edu>
2020-03-03 15:22:19 -0700

    dropdown component

---
+++ Project1/Assets/scripts/AtHomePractice/DropdownControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Color;
+
+namespace AtHomePractice
+{
+    [RequireComponent(typeof(Dropdown))]
+    public class DropdownControl : MonoBehaviour
+    {
+        private Dropdown controller;
+        
+        void Start()
+        {
+            controller = GetComponent<Dropdown>();
+            controller.options.Add(new Dropdown.OptionData("Hello"));
+            controller.options.Remove(new Dropdown.OptionData("Option A"));
+            controller.options.Remove(new Dropdown.OptionData("Option B"));
+            controller.options.Remove(new Dropdown.OptionData("Option C"));
+            controller.name = "Dialogue";
+        }
+
+        
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                controller.options.Add(new Dropdown.OptionData("Reach for the sky!"));
+            }
+        }
+    }
+}
