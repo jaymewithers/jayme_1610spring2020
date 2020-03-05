a770ce67c599701543adb4e5232c940e271e4f8a
Jayme Withers <10859571@my.uvu.edu>
2020-03-05 10:47:30 -0700

    username display

---
+++ Project1/Assets/scripts/AtHomePractice/Username.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class Username : MonoBehaviour
+    {
+        public string firstName;
+        public string lastName;
+        private string userName;
+        public GameObject player;
+
+        private void OnMouseUp()
+        {
+            print(UserInfo());
+            print(userName + " is your username.");
+            player.name = userName;
+        }
+
+        string UserInfo()
+        {
+            userName = firstName + lastName + 2000;
+            return firstName + " " + lastName + " are you ready to play?";
+        }
+    }
+}
+
+// anthony's github
