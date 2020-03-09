7149c2985f560dcf7eb7e05293f71733b346d562
Jayme Withers <10859571@my.uvu.edu>
2020-03-09 09:20:23 -0600

    weapon config

---
+++ Project1/Assets/scripts/AtHomePractice/WeaponHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace AtHomePractice
+{
+    public class WeaponHandler : MonoBehaviour
+    {
+        public WeaponConfig weaponObj;
+
+        void Start()
+        {
+            weaponObj.weaponFireAction = Fire;
+            var renderer = GetComponent<Renderer>();
+            renderer.material.color = weaponObj.weaponColor;
+        }
+
+        public void Fire()
+        {
+            var ammo = Instantiate(weaponObj.ammoObj);
+            var renderer = ammo.GetComponent<Renderer>();
+            renderer.material.color = weaponObj.RandomColor();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            weaponObj.RaiseCollectAction();
+        }
+    }
+}
+// continue with other related scripts (weaponconfig, weaponbuttonhandler, ammohandler) from 2670Fall2019
+
+// anthony's github
