7f1d661fe3fcb8af6caf92bc1dff562164357258
Jayme Withers <10859571@my.uvu.edu>
2020-03-11 11:13:56 -0600

    loops / lists / collectables

---
+++ Project1/Assets/scripts/ListAndLoops.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ListAndLoops : ScriptableObject
+{
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddToList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+    public void RemoveFromList(string stringObJ)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObJ)
+            {
+                stringList.Remove(stringObJ);
+            }
+        }
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+    
+    public void CheckList()
+    {
+        foreach (var obj in stringList)
+        {
+            if (obj != singleString)
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+}
