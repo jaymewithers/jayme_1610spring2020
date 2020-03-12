45df59dfc5cd9bda88b3b66098db30b1c1b45f06
Jayme Withers <10859571@my.uvu.edu>
2020-03-12 10:13:49 -0600

    weapon collectables

--- Project1/Assets/scripts/CollectableList.cs
+++ Project1/Assets/scripts/CollectableList.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class CollectableList : ScriptableObject
 {
     public List<Collectable> collectables;
-    
-    
+
+    public void AddToCollection(Collectable listObj)
+    {
+        collectables.Add(listObj);
+    }
+
+    public void RemoveFromCollection(Collectable listObj)
+    {
+        for (int i = 0; i < collectables.Count; i++)
+        {
+            collectables.Remove(listObj);
+        }
+    }
+
+    public void SortCollectables()
+    {
+        collectables.Sort();
+    }
 }
