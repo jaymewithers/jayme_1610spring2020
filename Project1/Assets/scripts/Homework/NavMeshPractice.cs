ca3df89176dd8844cf78f3d8695e429ee00cbe03
Jayme Withers <10859571@my.uvu.edu>
2020-03-26 10:11:07 -0600

    navmesh practice

---
+++ Project1/Assets/scripts/Homework/NavMeshPractice.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Homework
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NavMeshPractice : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        public Vector3 destination;
+        public float speed = 10f;
+        public float radius = 5f;
+        private float angularSpeed = 0f;
+        public Vector3 secondDestination;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = speed;
+            agent.radius = radius;
+            agent.angularSpeed = angularSpeed;
+            agent.nextPosition = secondDestination;
+        }
+
+        
+        void Update()
+        {
+            agent.destination = destination;
+        }
+    }
+}
