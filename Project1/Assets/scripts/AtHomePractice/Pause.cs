d76bce7ae31232a726033492ab4b12f7746e0477
Jayme Withers <10859571@my.uvu.edu>
2020-03-28 12:47:21 -0600

    Pause tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/Pause.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class Pause : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Time.timeScale = 0;
+                print("Paused.");
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Time.timeScale = 1;
+                print("Resumed.");
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Time.timeScale = 0.25f;
+                print("Slow motion.");
+            }
+        }
+    }
+}
+
+// unity tutorial - pause system
+// attach to camera
