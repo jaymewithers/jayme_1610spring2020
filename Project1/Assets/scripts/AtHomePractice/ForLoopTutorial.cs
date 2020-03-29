2c35e2942add856f29a980b172f2fa644630b159
Jayme Withers <10859571@my.uvu.edu>
2020-03-29 10:41:28 -0600

    for loops tutorial

---
+++ Project1/Assets/scripts/AtHomePractice/ForLoopTutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class ForLoopTutorial : MonoBehaviour
+    {
+        public int count;
+        
+        void Start()
+        {
+            StartCoroutine(AddToCount());
+        }
+
+        IEnumerator AddToCount()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                count++;
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+
+        private void Update()
+        {
+            if (count == 100)
+            {
+                print("Time's up.");
+            }
+        }
+    }
+}
+
+// unity tutorial - for loops
