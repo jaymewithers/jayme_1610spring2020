5a0ae62d46261773e992829e2cf4e23efcbb87e3
Jayme Withers <10859571@my.uvu.edu>
2020-04-01 11:28:52 -0600

    AI patrolling

---
+++ Project1/Assets/scripts/AlternateCoroutine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AlternateCoroutine : MonoBehaviour
+{
+    private WaitForFixedUpdate waitObj;
+    public bool canRun = true;
+    
+    void Start()
+    {
+        waitObj = new WaitForFixedUpdate();
+        StartCoroutine(OnStartCoroutine());
+    }
+
+    public IEnumerator OnStartCoroutine()
+    {
+        while (canRun)
+        {
+            yield return waitObj;
+            print("Repeating");
+        }
+    }
+
+    private void Update()
+    {
+        print("Updating");
+    }
+}
--- Project1/Assets/scripts/Behaviors/NavAgentBehavior.cs
+++ Project1/Assets/scripts/Behaviors/NavAgentBehavior.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
@@ -7,16 +8,45 @@ public class NavAgentBehavior : MonoBehaviour
     private NavMeshAgent agent;
     public Transform player;
     public float speed = 8f;
+    private Transform currentDestination;
+    private int i;
+    private bool canHunt;
+    
+    public List<Transform> patrolPoints;
     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
+        currentDestination = transform;
     }
 
- 
+    private void OnTriggerEnter(Collider other)
+    {
+        canHunt = true;
+        currentDestination = player;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canHunt = false;
+        currentDestination = transform;
+    }
+
     void Update()
     {
-        agent.destination = player.position;
+        //agent.destination = player.position;
+
+        if (canHunt)
+        {
+            agent.destination = currentDestination.position;
+            return;
+        }
+        
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            agent.destination = patrolPoints[i].position;
+            i = (i + 1) % patrolPoints.Count;
+        }
     }
 }
