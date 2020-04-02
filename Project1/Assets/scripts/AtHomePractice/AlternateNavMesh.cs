6fbc05f8990a87317f31c5376699e4e2f7946b0c
Jayme Withers <10859571@my.uvu.edu>
2020-04-02 11:53:59 -0600

    AI without patrolling

---
+++ Project1/Assets/scripts/AtHomePractice/AlternateNavMesh.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace AtHomePractice
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AlternateNavMesh : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        public Transform player;
+        public float speed = 8f;
+       
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = speed;
+        }
+
+        
+        void Update()
+        {
+            agent.destination = player.position;
+        }
+    }
+}
--- Project1/Assets/scripts/Behaviors/NavAgentBehavior.cs
+++ Project1/Assets/scripts/Behaviors/NavAgentBehavior.cs
@@ -2,51 +2,52 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
-public class NavAgentBehavior : MonoBehaviour
+namespace Behaviors
 {
-    private NavMeshAgent agent;
-    public Transform player;
-    public float speed = 8f;
-    private Transform currentDestination;
-    private int i;
-    private bool canHunt;
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NavAgentBehavior : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        public Transform player;
+        public float speed = 8f;
+        private Transform currentDestination;
+        private int i;
+        private bool canHunt;
     
-    public List<Transform> patrolPoints;
+        public List<Transform> patrolPoints;
     
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        agent.speed = speed;
-        currentDestination = transform;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        canHunt = true;
-        currentDestination = player;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        canHunt = false;
-        currentDestination = transform;
-    }
-
-    void Update()
-    {
-        //agent.destination = player.position;
-
-        if (canHunt)
+        void Start()
         {
-            agent.destination = currentDestination.position;
-            return;
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = speed;
+            currentDestination = transform;
         }
-        
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+
+        private void OnTriggerEnter(Collider other)
         {
-            agent.destination = patrolPoints[i].position;
-            i = (i + 1) % patrolPoints.Count;
+            canHunt = true;
+            currentDestination = player;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            canHunt = false;
+            currentDestination = transform;
+        }
+
+        void Update()
+        {
+            if (canHunt)
+            {
+                agent.destination = currentDestination.position;
+                return;
+            }
+        
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                agent.destination = patrolPoints[i].position;
+                i = (i + 1) % patrolPoints.Count;
+            }
         }
     }
 }
