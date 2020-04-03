9122bb12b609408a69c79ba26bf1823101386635
Jayme Withers <10859571@my.uvu.edu>
2020-04-03 09:48:54 -0600

    enemy states

---
+++ Project1/Assets/scripts/AtHomePractice/EnemyAI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Behaviors;
+using UnityEngine;
+
+namespace AtHomePractice
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        public enum EnemyStates
+        {
+            Patrolling,
+            Chasing,
+            Attacking,
+            Defeated
+        }
+
+        public EnemyStates currentEnemyState;
+    
+        void Start()
+        {
+            currentEnemyState = EnemyStates.Chasing;
+        }
+
+    
+        void Update()
+        {
+            switch (currentEnemyState)
+            {
+                case EnemyStates.Patrolling:
+                    print("Patrolling");
+                    break;
+                case EnemyStates.Chasing:
+                    print("Chasing");
+                    break;
+                case EnemyStates.Attacking:
+                    print("Attacking");
+                    break;
+                case EnemyStates.Defeated:
+                    print("Defeated");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
+
+// unity tutorial
