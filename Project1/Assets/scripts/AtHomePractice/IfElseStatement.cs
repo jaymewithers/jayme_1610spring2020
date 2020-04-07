fcefea837afd316686a6ecd57b4818d024251c11
Jayme Withers <10859571@my.uvu.edu>
2020-04-07 10:27:44 -0600

    if else statement (levels)

---
+++ Project1/Assets/scripts/AtHomePractice/IfElseStatement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AtHomePractice
+{
+    [RequireComponent(typeof(Text))]
+    public class IfElseStatement : MonoBehaviour
+    {
+        private Text textObj;
+        public int levelOne = 0;
+        public int levelTwo = 1;
+        public int levelThree = 2;
+        public int currentLevel;
+
+        void Start()
+        {
+            textObj = GetComponent<Text>();
+            currentLevel = levelOne;
+        }
+
+        
+        void Update()
+        {
+            if (currentLevel == levelOne)
+            {
+                textObj.text = "Level One";
+            }
+            else if (currentLevel == levelTwo)
+            {
+                textObj.text = "Level Two";
+            }
+            else if (currentLevel == levelThree)
+            {
+                textObj.text = "Level Three";
+            }
+            else
+            {
+                textObj.text = "Invalid";
+            }
+        }
+    }
+}
