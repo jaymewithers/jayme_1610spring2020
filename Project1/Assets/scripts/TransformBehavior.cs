b7a84a12e2221e63bbfc9e2b222539f8c7776da9
Jayme Withers <10859571@my.uvu.edu>
2020-04-15 11:19:21 -0600

    Platforms

---
+++ Project1/Assets/scripts/TransformBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TransformBehavior : MonoBehaviour
+{
+    public string otherTagString;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(otherTagString)) return;
+            transform.parent = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}
