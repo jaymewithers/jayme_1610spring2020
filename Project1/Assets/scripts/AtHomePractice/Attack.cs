b7a62e91c4cd874f774d322f3add6a96f8a5779c
Jayme Withers <10859571@my.uvu.edu>
2020-04-18 14:09:57 -0600

    Attack / game tweaks

---
+++ Project1/Assets/scripts/AtHomePractice/Attack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Health enemy, enemy2, enemy3, enemy4, enemy5;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            enemy.healthValue--;
+            enemy2.healthValue--;
+            enemy3.healthValue--;
+            enemy4.healthValue--;
+            enemy5.healthValue--;
+        }
+    }
+}
