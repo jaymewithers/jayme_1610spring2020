f312d70f746c1be7d9c186af8a4442f368ef90ea
Jayme Withers <10859571@my.uvu.edu>
2020-04-21 13:20:10 -0600

    practice and game tweaks

--- Project1/Assets/scripts/AtHomePractice/Attack.cs
+++ Project1/Assets/scripts/AtHomePractice/Attack.cs
@@ -5,13 +5,11 @@ public class Attack : MonoBehaviour
     public Health enemy, enemy2, enemy3, enemy4, enemy5;
     private void OnTriggerEnter(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            enemy.healthValue--;
-            enemy2.healthValue--;
-            enemy3.healthValue--;
-            enemy4.healthValue--;
-            enemy5.healthValue--;
-        }
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        enemy.healthValue--;
+        enemy2.healthValue--;
+        enemy3.healthValue--;
+        enemy4.healthValue--;
+        enemy5.healthValue--;
     }
 }
---
+++ Project1/Assets/scripts/AtHomePractice/NavAgentBehaviorPractice.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentBehaviorPractice : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    public float speed = 8f;
+    private Transform currentDestination;
+    private int i;
+    public bool canHunt;
+
+    public List<Transform> patrolPoints;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        currentDestination = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canHunt = true;
+        currentDestination = player;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canHunt = false;
+        currentDestination = transform;
+    }
+
+
+    void Update()
+    {
+        transform.LookAt(agent.steeringTarget);
+        if (canHunt)
+        {
+            agent.destination = currentDestination.position;
+            return;
+        }
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            agent.destination = patrolPoints[i].position;
+            i = (i + 1) % patrolPoints.Count;
+        }
+    }
+}
